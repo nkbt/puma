4815d8d34d328cdf01eea56633dfb16c1f51d26b
Nikita Butenko <nikita.butenko@gmail.com>
2013-02-25 15:29:03 +1100

    Libraries rearrange

--- index.js
+++ index.js
@@ -1,3 +1,1 @@
-module.exports = process.env.PUMA_COV
-  ? require('./lib-cov/puma')
-  : require('./lib/puma');
+module.exports = process.env.PUMA_COV ? require('./lib-cov/puma') : require('./lib/puma');
